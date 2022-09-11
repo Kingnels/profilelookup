c8bd163dadaad2c056b64028b7faf6fc10492bc0
Kingnels <ifeanyiibe2022@gmail.com>
2022-09-11 20:07:39 +0100

    more on loops

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ var contact = [
 },
 
 {
-    "firstName": "Kristin",
+    "firstName": "kristin",
     "lastName": "Charmes",
 
     "number": "09107878682",
@@ -31,15 +31,37 @@ var contact = [
 
 function lookUpProfile (name, property){
 
-    for (var i = 0; i < contact.length; item++){
+    for (var i = 0; i < contact.length; i++){
     
         if(contact[i].firstName == name){
             return contact[i][property] || "no such property"
+            // return contact[i][property]
         }
     }
     return "no such contact"
 }
 
-var data = lookUpProfile("Akira","number")
+var data = lookUpProfile("Akira","likes")
 
 console.log(data)
+
+var bar = [4,6,7,3,2]
+var total = 0
+
+for (i = 0; i < bar.length;i++){
+    total = total + bar[i]
+}
+console.log(total)
+
+var product = 1
+function multiply(arr){
+    for (var i = 0; i < arr.length; i++){
+        for (var j = 0; j < arr[i].length; j++){
+            product *= arr[i][j] 
+        }
+    }
+    return  product 
+}
+
+var holme = multiply([[2,4],[1,4],[5,4],[6,4],[8,4]])
+console.log(holme)
